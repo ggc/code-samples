f56b47c776995352828e25a71c722d067fc05acb
Gabriel <gabriel.galan.17@gmail.com>
2015-12-15 23:52:37 +0100

    open terminado

--- pr3/bParte/fifoproc.c
+++ pr3/bParte/fifoproc.c
@@ -1,3 +1,4 @@
+#include <linux/semaphore.h>
 #include "cbuffer.h"
 
 MODULE_LICENSE("GPL");
@@ -13,31 +14,46 @@ int nr_cons_waiting=0;
 
 /* Funciones de inicialización y descarga del módulo */
 int init_module(void){
-	cbuffer = create_cbuffer_t(50);
-	sema_init(mtx,1);
-	sema_init(sem_prod,X);
-	sema_init(sem_cons,Y);
+  cbuffer = create_cbuffer_t(50);
+  sema_init(mtx,1);
+  sema_init(sem_prod,50);
+  sema_init(sem_cons,0);
+  nr_prod_waiting=0;
+  nr_cons_waiting=0;
+  prod_count=0;
+  cons_count=0;
 }
 void cleanup_module(void){
-
+  destroy_cbuffer_t(cbuffer);
 }
 /* Se invoca al hacer open() de entrada /proc */
 static int fifoproc_open(struct inode * inode, struct file * fd){
 
- return 0;
+  if(file->f_mode & FMODE_READ){ //El consumidor abre el FIFO
+    while(prod_count==0) //BEH:Si no hay prod, bloquea
+      down_interruptible(&sem_cons);
+    cons_count++;
+    up(&sem_prod);
+  } else{ //El productor abre el FIFO
+    while(cons_count==0) //BEH:Si no hay cons, bloquea
+      down_interruptible(&sem_prod);
+    prod_count++;
+    up(&sem_cons);
+  }
+  return 0;
 }
 /* Se invoca al hacer close() de entrada /proc */
 static int fifoproc_release(struct inode * inode, struct file * fd){
 
 
-return 0;
+  return 0;
 }
 /* Se invoca al hacer read() de entrada /proc */
 static ssize_t fifoproc_read(struct file * fd, char * rbuffer, size_t size, loff_t * offset){
-
+  //if(nr_gaps_cbuffer_t(cbuffer)<size)
+    
 }
 /* Se invoca al hacer write() de entrada /proc */
-static ssize_t fifoproc_write(struct file * fd, const char * wbuffer, size_t size,
-loff_t * offset){
+static ssize_t fifoproc_write(struct file * fd, const char * wbuffer, size_t size, loff_t * offset){
 
 }
