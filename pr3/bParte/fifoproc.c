1ac58098b6729f6950e84d3c6a3051c8f6cb91e7
Gabriel <gabriel.galan.17@gmail.com>
2015-12-16 04:30:10 +0100

    At least it compiles...

--- pr3/bParte/fifoproc.c
+++ pr3/bParte/fifoproc.c
@@ -1,8 +1,19 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/proc_fs.h>
+#include <linux/string.h>
+#include <linux/vmalloc.h>
+#include <linux/init.h>
+#include <asm-generic/uaccess.h>
+#include <asm-generic/errno.h>
 #include <linux/semaphore.h>
 #include "cbuffer.h"
 
-MODULE_LICENSE("GPL");
 
+
+//MODULE_LICENSE("GPL");
+
+static struct proc_dir_entry *proc_entry;
 cbuffer_t* cbuffer;
 int prod_count = 0;
 int cons_count = 0;
@@ -14,25 +25,10 @@ struct semaphore sem_consReady;
 int nr_prod_waiting=0;
 int nr_cons_waiting=0;
 
-/* Funciones de inicialización y descarga del módulo */
-int init_module(void){
-  cbuffer = create_cbuffer_t(50);
-  sem_init(mtx,1);
-  sem_init(sem_prod,0);
-  sem_init(sem_cons,0);
-  sem_init(sem_consReady,0);
-  sem_init(sem_prodReady,0);
-  nr_prod_waiting=0;
-  nr_cons_waiting=0;
-  prod_count=0;
-  cons_count=0;
-}
-void cleanup_module(void){
-  destroy_cbuffer_t(cbuffer);
-}
 /* Se invoca al hacer open() de entrada /proc */
-static int fifoproc_open(struct inode * inode, struct file * fd){
-  
+static int fifoproc_open(struct inode * inode, struct file *fd){
+
+  printk("fifoopen\n");  
   if(fd->f_mode & FMODE_READ){ //El consumidor abre el FIFO
     //Traduccion START
     if(down_interruptible(&mtx)) //Lock
@@ -58,7 +54,7 @@ static int fifoproc_open(struct inode * inode, struct file * fd){
     //Traduccion START
     if(down_interruptible(&mtx)) //Lock
       return -EINTR;
-    while(cons==0 && nr_cons_waiting==0) {
+    while(cons_count==0 && nr_cons_waiting==0) {
       nr_prod_waiting++;
       up(&mtx);
       //Codigo de seguridad//
@@ -80,6 +76,8 @@ static int fifoproc_open(struct inode * inode, struct file * fd){
 }
 /* Se invoca al hacer close() de entrada /proc */
 static int fifoproc_release(struct inode * inode, struct file * fd){
+  
+  printk("fiforelease\n");
   if(fd->f_mode & FMODE_READ){ //El consumidor cierra el FIFO
     down_interruptible(&mtx);
     cons_count--;
@@ -94,9 +92,159 @@ static int fifoproc_release(struct inode * inode, struct file * fd){
 /* Se invoca al hacer read() de entrada /proc */
 static ssize_t fifoproc_read(struct file * fd, char * rbuffer, size_t size, loff_t * offset){
   //if(nr_gaps_cbuffer_t(cbuffer)<size)
+
+  char kbuf[51];
+  int nr_bytes=0;
+  char * item;
+
+
+  printk("fiforead\n");  
+  item = vmalloc(sizeof(char));
+  if(offset>0)
+    return 0;
+  if(down_interruptible(&mtx)){
+    return -EINTR;
+  }
+  while(size_cbuffer_t(cbuffer)<size){
+    nr_cons_waiting++;
+    up(&mtx);
+    if (down_interruptible(&sem_cons)){
+      down(&mtx);
+      nr_cons_waiting--;
+      up(&mtx);
+      return -EINTR;
+    }
+
+    if (down_interruptible(&mtx)){
+      return -EINTR;
+    }
+  }
+
   
+  item = head_cbuffer_t(cbuffer);
+  remove_cbuffer_t(cbuffer);
+  printk("printk %s",item);
+  if (nr_prod_waiting>0)
+    {
+      up(&sem_prod);
+      nr_prod_waiting--;
+    }
+
+  up(&mtx);
+
+  nr_bytes=sprintf(kbuf, "%s",item);
+
+  vfree(item);
+
+  if (size < nr_bytes)
+    return -ENOSPC;
+  
+  if (copy_to_user(rbuffer,kbuf,nr_bytes))
+    return -EINVAL;
+   
+  (*offset)+=nr_bytes;
+
+  return nr_bytes;
 }
 /* Se invoca al hacer write() de entrada /proc */
 static ssize_t fifoproc_write(struct file * fd, const char * wbuffer, size_t size, loff_t * offset){
+  char kbuf[51];
+  char val=' ';
+  int* item=NULL;
+
+
+  printk("fifowrite\n");
+  if (size > 50) {
+    return -ENOSPC;
+  }
+  if (copy_from_user( kbuf, wbuffer, size )) {
+    return -EFAULT;
+  }
+  //strcat(kbuf, '\0');
+  *offset+=size;
+
+
+  if (sscanf(kbuf,"%c",&val)!=1){
+    return -EINVAL;
+  }
+
+  //item=vmalloc(sizeof(char));
+  //item=val;
+
+  if(down_interruptible(&mtx)){
+    vfree(item);
+    return -EINTR;
+  }
+  while(nr_gaps_cbuffer_t(cbuffer) < size && cons_count <= 0){
+   
+    printk("nr_prod_waiting\n"); 
+    nr_prod_waiting++;
+    up(&mtx);
+    if(down_interruptible(&sem_prod)){
+      down(&mtx);
+      vfree(item);
+      nr_prod_waiting--;
+      up(&mtx);
+      return -EINTR;
+    }
+    if(down_interruptible(&mtx)){
+      vfree(item);
+      return -EINTR;
+    }
+  }
   
+  printk("fifo preinsercion %s\n",val);
+
+  insert_cbuffer_t(cbuffer, val); //item
+
+  if(nr_cons_waiting>0){
+    up(&sem_cons);
+    nr_cons_waiting--;
+  }
+
+  up(&mtx);
+
+  return size;
 }
+ 
+static const struct file_operations proc_entry_fops = {
+  .open = fifoproc_open,
+  .release = fifoproc_release,
+  .read = fifoproc_read,
+  .write = fifoproc_write,    
+};
+
+/* Funciones de inicialización y descarga del módulo */
+int init_fifoproc_module(void){
+  sema_init(&mtx,1);
+  sema_init(&sem_prod,0);
+  sema_init(&sem_cons,0);
+  sema_init(&sem_consReady,0);
+  sema_init(&sem_prodReady,0);
+  cbuffer = create_cbuffer_t(50);
+  nr_prod_waiting=0;
+  nr_cons_waiting=0;
+  prod_count=0;
+  cons_count=0;
+
+  if(!cbuffer)
+    return -ENOMEM;
+
+  proc_entry = proc_create_data("modfifo",0666, NULL, &proc_entry_fops,NULL);
+  
+  if(proc_entry==NULL){
+    destroy_cbuffer_t(cbuffer);
+    printk(KERN_INFO "prodcons: Can't create /proc entry\n");
+    return -ENOMEM;
+  } else {
+    printk(KERN_INFO "prodcons: Module loaded\n");
+  }
+  return 0;
+}
+void cleanup_fifoproc_module(void){
+  remove_proc_entry("modfifo", NULL);
+  destroy_cbuffer_t(cbuffer);
+}
+
+module_init( init_fifoproc_module );
+module_exit( cleanup_fifoproc_module );
