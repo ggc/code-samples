05a70621439c86bb5876e3a14d97e884c7161fcd
oka-haist <gabriel.galan.17@gmail.com>
2016-06-01 12:15:46 +0200

    Release first deployable version on Heroku

--- loc8r/app_server/routes/index.js
+++ loc8r/app_server/routes/index.js
@@ -2,12 +2,6 @@ var express = require('express');
 var router = express.Router();
 var ctrlMain = require('../controllers/main');
 
-/* GET home page. */
-var homepageController = function(req, res) {
-  res.render('index', { title: 'Express' });
-};  
-
-
 router.get('/', ctrlMain.index);
 
 module.exports = router;
