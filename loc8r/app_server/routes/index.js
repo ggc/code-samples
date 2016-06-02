9b9cebdeeb05b16d4438923f55764185140d0277
oka-haist <gabriel.galan.17@gmail.com>
2016-06-02 15:22:38 +0200

    Add loc9r basic view and routes

--- loc8r/app_server/routes/index.js
+++ loc8r/app_server/routes/index.js
@@ -1,7 +1,14 @@
 var express = require('express');
 var router = express.Router();
-var ctrlMain = require('../controllers/main');
+var ctrlLocations = require('../controllers/locations.js');
+var ctrlOthers = require('../controllers/others.js');
 
-router.get('/', ctrlMain.index);
+/* Location-like pages*/
+router.get('/', ctrlLocations.homelist);
+router.get('/location', ctrlLocations.locationInfo);
+router.get('/location/review/new', ctrlLocations.addReview);
+
+/* Other pages */
+router.get('/about', ctrlOthers.about);
 
 module.exports = router;
