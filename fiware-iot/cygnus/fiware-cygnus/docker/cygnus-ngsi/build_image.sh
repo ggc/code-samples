2b999b0921ab460897d8ae726666037b78f7159f
G GC <ggalan@ucm.es>
2017-05-18 14:47:18 +0200

    Add a compile-able and runnable cygnus with grouping rules

---
+++ fiware-iot/cygnus/fiware-cygnus/docker/cygnus-ngsi/build_image.sh
@@ -0,0 +1,4 @@
+sudo docker build -f ./docker/cygnus-ngsi/Dockerfile -t cygnus-ngsi .
+
+sudo docker run --name cygnus 
+
---
+++ fiware-iot/cygnus/fiware-cygnus/start_cygnus.sh
@@ -0,0 +1,2 @@
+docker run --name centos-manual --network=fiware-network --ip=172.18.0.5 -p 80:80 -it centos:cygnus
+
