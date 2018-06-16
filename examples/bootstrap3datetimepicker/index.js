cc5186b0fbb2d9525242374d2acdcfe6218713ef
Gabriel GC <gabriel.galancasillas@outlook.com>
2018-06-16 15:33:59 +0200

    refactor: Mv snsible and boostrap projects

--- examples/bootstrap3datetimepicker/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-var server = require('http').createServer(app);
-
-app.get( '/', function(req, res, next) {
-	res.sendFile(__dirname + '/index.html');
-});
-
-server.listen(3000);
