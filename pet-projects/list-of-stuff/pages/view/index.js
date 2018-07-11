fa17242594c2de607204a16ee4d5c49f2f11293b
Gabriel GC <gabriel.galancasillas@outlook.com>
2018-07-12 00:15:55 +0200

    feat(list-of-stuff): Add basic state to component

---
+++ pet-projects/list-of-stuff/pages/view/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+    StyleSheet,
+    Text,
+    View
+} from 'react-native';
+import Button from 'apsl-react-native-button';
+import PropType from 'prop-types';
+
+import container from './container';
+
+
+
+export default container(App);
