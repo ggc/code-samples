6979a501b40ed99e28d306a598991dab97d42184
Gabriel GC <gabriel.galan.17@gmail.com>
2018-07-14 01:04:30 +0200

    feat(list-of-stuff): Trying eject

--- pet-projects/list-of-stuff/pages/Footer.js
+++ pet-projects/list-of-stuff/pages/Footer.js
@@ -1,48 +1,24 @@
 import React from 'react';
-import { FlatList, View, Text, TextInput } from 'react-native';
-// import { listRepos } from '../reducers/reducer';
-// import { connect } from 'react-redux';
+import { StyleSheet, Text, View } from 'react-native';
 
-class Footer extends React.Component {
-    // constructor(props) {
-    //     super(props);
-    //     console.log('[LOG] constructor props', props);
-    //     this.handleChange = this.handleChange.bind(this);
-    // }
-    // componentDidMount() {
-    //     console.log('[LOG] this at Footer', this);
-    //     // this.props.listRepos('relferreira');
-    // }
-    
-    // renderItem = ({ item }) => (
-    //     <View>
-    //         <Text>{item.name}</Text>
-    //     </View>
-    // );
+export default class Footer extends React.Component {
     render() {
-        console.log('[LOG] this', this);
+        const date = new Date();
         return (
-            <View>
-                <Text>I'm a footer</Text>
+            <View style={styles.container}>
+                <Text>Im the footer</Text>
             </View>
         );
     }
-    // When input changes, handleChange fires and
-    // the onTextWritten handler my father gave me
-    // fires with my text. Too much callback-like 
-    handleChange(e) {
-        console.log('[LOG] text changed', e);
-        // this.props.onTextWritten(e.target.value);
-    }
-    
-    // render() {
-    //     return (
-    //         <View>
-    //             <Text> {this.props.options}</Text>
-    //             <TextInput onChangeText={() => console.log('Text chenaged!')} />
-    //         </View>
-    //         )
-    // }
 }
 
-module.exports = Footer;
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#00a1ff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: 100,
+        width: 5000
+    },
+});
--- pet-projects/list-of-stuff/pages/view/container.js
+++
@@ -1,9 +0,0 @@
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
-import { usdPrice, loading } from '../state/selectors';
-import { priceRequest } from '../state/price/actions';
-
-export default connect(
-    createStructuredSelector({ loading, usdPrice }),
-    { priceRequest }
-);
