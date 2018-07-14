42d401c2ebfd2c0ae3cb964f24d6182cb66a6efd
Gabriel GC <gabriel.galancasillas@outlook.com>
2018-07-14 12:53:08 +0200

    feat(list-of-stuff): linter + login screen[WIP]

--- pet-projects/list-of-stuff/App.js
+++ pet-projects/list-of-stuff/App.js
@@ -1,32 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Footer from './pages/Footer';
-// import { applyMiddleware } from "redux";
-// import { Provider, connect } from "react-redux";
-// import { createStore } from 'redux';
-
-// import reducer from "./reducers/reducer";
-// import getStore from "./pages/state/reducer";
-// import 'es6-symbol/implement';
-// const store = createStore(reducer);
-// const store = getStore();
-function reducer(state, action) {
-    return state;
-}
-// var store = createStore(reducer);
+import LoginPage from './pages/login/login-page';
 
 export default class App extends React.Component {
     render() {
         const date = new Date();
         return (
-                <View style={styles.container}>
-                    <Text style={styles.lightBlue}>Header</Text>
-                    <Text>{date.toString()}</Text>
-                    <Text>Open up App.js to start working on your app!</Text>
-                    <Text>Live reload ON!.</Text>
-                    <Text>Shake (twice...) your phone to open the developer menu.</Text>
-                    <Footer />
-                </View>
+            <View style={styles.container}>
+                <LoginPage />
+            </View>
         );
     }
 }
